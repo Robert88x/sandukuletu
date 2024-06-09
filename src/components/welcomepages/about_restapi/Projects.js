4cb60ee56821f07011c263cfcc2c9e2e77fe7b35
Robert Makanyaga <ropamakanyaga@gmail.com>
2024-06-09 23:27:10 +0300

    adding project rest api

---
+++ src/components/welcomepages/about_restapi/Projects.js
@@ -0,0 +1,32 @@
+export const PROJECTS = [
+    {
+        id: 1,
+        name: "Kitaa",
+        description: "Chati za mitaa na serikali za mitaa",
+        date: "2022"
+    },
+    {
+        id: 2,
+        name: "Ofisiboxi",
+        description: "Chati za ofisini",
+        date: "2023"
+    },
+    {
+        id: 3,
+        name: "Afyaboxi",
+        description: "Rekodi za wagonjwa karibu yako",
+        date: "2024"
+    },
+    {
+        id: 4,
+        name: "Vitabuboxi",
+        description: "waandishi wa vitabu",
+        date: "2025"
+    },
+    {
+        id: 5,
+        name: "Ajiraboxi",
+        description: "centralized update folio na kupata ajira",
+        date: "2026"
+    },
+];
